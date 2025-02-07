3d16bd70cf5fcbb6f116030a6df8cad75da16fff
Daniel P Pflager <daniel.pflager@gmail.com>
2025-02-06 20:34:28 -0800

    progress

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+import os
+
+app = FastAPI()
+
+# Serve files from the current directory
+app.mount("/", StaticFiles(directory=".", html=True), name="static")
+
+uvicorn.run(app, host="127.0.0.1", port=8888)
